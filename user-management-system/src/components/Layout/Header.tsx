a14b09eb7f754b1ab8728146607d26481f6f7853
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-08-15 02:03:53 +0900

    test

--- user-management-system/src/components/Layout/Header.tsx
+++ user-management-system/src/components/Layout/Header.tsx
@@ -4,6 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../redux/slices/authSlice';
 import { RootState } from '../../redux/store';
 import "../../App.css"
+import { IconButton } from '@mui/material';
+import LoginIcon from '@mui/icons-material/Login';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 const Header: React.FC = () => {
   const dispatch = useDispatch();
@@ -17,8 +20,16 @@ const Header: React.FC = () => {
     <header className='header'>
       <h1>React Redux User Management System</h1>
       <nav>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        <Link to="/login">
+        <IconButton>
+          <LoginIcon />
+        </IconButton>
+        </Link>
+        <Link to="/register">
+        <IconButton>
+          <PersonAddIcon />
+        </IconButton>
+        </Link>
         {isAuthenticated && <Link to="/profile">Profile</Link>}
         {isAuthenticated && <button onClick={handleLogout}>Logout</button>}
       </nav>
