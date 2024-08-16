3bfbc6a89425250cb8f333d66d9a46687d02f8c0
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-08-17 04:29:30 +0900

    test

--- user-management-system/src/components/Layout/UserList.tsx
+++ user-management-system/src/components/Layout/UserList.tsx
@@ -2,35 +2,39 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { setUsers } from '../../redux/slices/userSlice';
+import axios from 'axios';
 
 const UserList: React.FC = () => {
   const dispatch = useDispatch();
   const users = useSelector((state: RootState) => state.user.users);
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => dispatch(setUsers(data)))
-      .catch(error => console.error('Error fetching users:', error));
+    axios.get('http://localhost:3000/api/users')
+      .then(response => {
+        dispatch(setUsers(response.data));
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+      });
   }, [dispatch]);
 
   return (
     <div>
       <h2>User List</h2>
-      <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+      <table className='tbl'>
         <thead>
           <tr>
-            <th style={{ border: '1px solid black', padding: '8px' }}>ID</th>
-            <th style={{ border: '1px solid black', padding: '8px' }}>Username</th>
-            <th style={{ border: '1px solid black', padding: '8px' }}>Email</th>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Email</th>
           </tr>
         </thead>
         <tbody>
           {users.map(user => (
             <tr key={user.id}>
-              <td style={{ border: '1px solid black', padding: '8px' }}>{user.id}</td>
-              <td style={{ border: '1px solid black', padding: '8px' }}>{user.username}</td>
-              <td style={{ border: '1px solid black', padding: '8px' }}>{user.email}</td>
+              <td>{user.id}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
             </tr>
           ))}
         </tbody>
