4078daffcd30b8def7464b99edb7a737a1860e52
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-08-24 19:04:38 +0900

    test

--- user-management-system/src/components/Layout/Sidebar.tsx
+++ user-management-system/src/components/Layout/Sidebar.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../redux/store';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { IconButton } from "@mui/material";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 const Sidebar: React.FC = () => {
   const location = useLocation();
@@ -9,18 +12,25 @@ const Sidebar: React.FC = () => {
   const isActive = (path: string) => location.pathname === path;
 
   return (
-    <aside className='aside'>
-      <ul className='aside_list'>
+    <aside className="aside">
+      <ul className="aside_list">
         {user && (
-          <li className='aside_list_item'>
-            <Link to="/dashboard" className={isActive('/dashboard') ? 'open' : ''}>
-              Dashboard
+          <li className="aside_list_item">
+            <Link
+              to="/dashboard"
+              className={isActive("/dashboard") ? "open" : ""}
+            >
+              <IconButton aria-label="Home">
+                <DashboardIcon />
+              </IconButton>
             </Link>
           </li>
         )}
-        <li className='aside_list_item'>
-          <Link to="/settings" className={isActive('/settings') ? 'open' : ''}>
-            Settings
+        <li className="aside_list_item">
+          <Link to="/settings" className={isActive("/settings") ? "open" : ""}>
+            <IconButton aria-label="Home">
+              <SettingsIcon />
+            </IconButton>
           </Link>
         </li>
       </ul>
